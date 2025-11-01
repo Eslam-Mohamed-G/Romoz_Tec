bfc6e5b160ba69a294aef7dcdfe32d041b96cda5
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-11-02 00:42:13 +0200

    Add UserSettings

--- src/Pages/UserProfile/UserSettings/UserSettings.jsx
+++ src/Pages/UserProfile/UserSettings/UserSettings.jsx
@@ -85,73 +85,85 @@ export default function UserSettings() {
             </header>
 
             <div className="userSettings_body">
-                <div className="user_data">
-                    <div className="user_images">
-                        <div className="cover_user_image">
-                            {userData?.user?.cover_image ?
-                                <img src={userData?.user?.cover_image} alt="img cover" loading="lazy" />
-                                :
-                                <svg className="skeleton-svg" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
-                                    <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
-                                    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
-                                </svg>
-                            }
+                <div className="userSettings_body_header">
+                    <ul className='userSettings_body_header_content'>
+                        <li><span>حسابي</span></li>
+                        <li><span>إضافة عنوان</span></li>
+                        <li><span>الخصوصية</span></li>
+                        <li><span>حذف الحساب</span></li>
+                        <li><span>الشروط والأحكام</span></li>
+                    </ul>
+                </div>
 
-                            <label className="change_banner_btn">
-                                <input
-                                    type="file"
-                                    accept="image/*"
-                                    onChange={(e) =>
-                                        handleImageUpload(
-                                            e,
-                                            "cover_image",
-                                            "https://api.mashy.sand.alrmoz.com/api/cover-image"
-                                        )
-                                    }
-                                    disabled={imageLoading}
-                                />
-                                <div className="change_banner_content">
-                                    <div className="change_banner_icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width={23} height={23} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-camera-icon lucide-camera"><path d="M13.997 4a2 2 0 0 1 1.76 1.05l.486.9A2 2 0 0 0 18.003 7H20a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h1.997a2 2 0 0 0 1.759-1.048l.489-.904A2 2 0 0 1 10.004 4z" /><circle cx={12} cy={13} r={3} /></svg>
-                                    </div>
-                                    <span> تغيير البانر</span>
-                                </div>
-                            </label>
-                        </div>
-
-                        <div className="profile_user_image">
-                            <div className="user_img_container">
-                                {userData?.user?.profile_image ?
-                                    <img src={userData?.user?.profile_image} alt="img" loading="lazy" />
+                <div className="userSettings_body_container">
+                    <div className="user_data">
+                        <div className="user_images">
+                            <div className="cover_user_image">
+                                {userData?.user?.cover_image ?
+                                    <img src={userData?.user?.cover_image} alt="img cover" loading="lazy" />
                                     :
-                                    <svg className="skeleton-user-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20" >
-                                        <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                                    <svg className="skeleton-svg" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+                                        <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                                        <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
                                     </svg>
                                 }
+
+                                <label className="change_banner_btn">
+                                    <input
+                                        type="file"
+                                        accept="image/*"
+                                        onChange={(e) =>
+                                            handleImageUpload(
+                                                e,
+                                                "cover_image",
+                                                "https://api.mashy.sand.alrmoz.com/api/cover-image"
+                                            )
+                                        }
+                                        disabled={imageLoading}
+                                    />
+                                    <div className="change_banner_content">
+                                        <div className="change_banner_icon">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width={23} height={23} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-camera-icon lucide-camera"><path d="M13.997 4a2 2 0 0 1 1.76 1.05l.486.9A2 2 0 0 0 18.003 7H20a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h1.997a2 2 0 0 0 1.759-1.048l.489-.904A2 2 0 0 1 10.004 4z" /><circle cx={12} cy={13} r={3} /></svg>
+                                        </div>
+                                        <span> تغيير البانر</span>
+                                    </div>
+                                </label>
                             </div>
-                            <label className="profile_camera_btn">
-                                <div className="profile_camera_icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width={23} height={23} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-camera-icon lucide-camera"><path d="M13.997 4a2 2 0 0 1 1.76 1.05l.486.9A2 2 0 0 0 18.003 7H20a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h1.997a2 2 0 0 0 1.759-1.048l.489-.904A2 2 0 0 1 10.004 4z" /><circle cx={12} cy={13} r={3} /></svg>
-                                </div>
 
-                                <input
-                                    type="file"
-                                    accept="image/*"
-                                    onChange={(e) =>
-                                        handleImageUpload(
-                                            e,
-                                            "profile_image",
-                                            "https://api.mashy.sand.alrmoz.com/api/profile-image"
-                                        )
+                            <div className="profile_user_image">
+                                <div className="user_img_container">
+                                    {userData?.user?.profile_image ?
+                                        <img src={userData?.user?.profile_image} alt="img" loading="lazy" />
+                                        :
+                                        <svg className="skeleton-user-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20" >
+                                            <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                                        </svg>
                                     }
-                                    disabled={imageLoading}
-                                />
-                            </label>
-                        </div>
-                    </div>
+                                </div>
+                                <label className="profile_camera_btn">
+                                    <div className="profile_camera_icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width={23} height={23} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-camera-icon lucide-camera"><path d="M13.997 4a2 2 0 0 1 1.76 1.05l.486.9A2 2 0 0 0 18.003 7H20a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h1.997a2 2 0 0 0 1.759-1.048l.489-.904A2 2 0 0 1 10.004 4z" /><circle cx={12} cy={13} r={3} /></svg>
+                                    </div>
 
-                    <div className="user_info">
-                        <h3 className="user_name">{userData?.user?.name?.split(" ").slice(0, 2).join(" ")}</h3>
+                                    <input
+                                        type="file"
+                                        accept="image/*"
+                                        onChange={(e) =>
+                                            handleImageUpload(
+                                                e,
+                                                "profile_image",
+                                                "https://api.mashy.sand.alrmoz.com/api/profile-image"
+                                            )
+                                        }
+                                        disabled={imageLoading}
+                                    />
+                                </label>
+                            </div>
+                        </div>
+
+                        <div className="user_info">
+                            <h3 className="user_name">{userData?.user?.name?.split(" ").slice(0, 2).join(" ")}</h3>
+                        </div>
                     </div>
                 </div>
             </div>
