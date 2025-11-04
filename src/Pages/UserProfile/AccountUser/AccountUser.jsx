cfafb6ecc0fffea2d960256113589f93339987ee
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-11-04 23:46:55 +0200

    add css style to UserProfile page

--- src/Pages/UserProfile/AccountUser/AccountUser.jsx
+++ src/Pages/UserProfile/AccountUser/AccountUser.jsx
@@ -20,8 +20,8 @@ export default function AccountUser() {
                 <div className="user_data">
                     <div className="user_images">
                         <div className="cover_user_image">
-                            {userData?.user?.cover_image ?
-                                <img src={userData?.user?.cover_image} alt="img cover" loading="lazy" />
+                            {userData?.cover_image ?
+                                <img src={userData?.cover_image} alt="img cover" loading="lazy" />
                                 :
                                 <svg className="skeleton-svg" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
                                     <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
@@ -32,8 +32,8 @@ export default function AccountUser() {
 
                         <div className="profile_user_image">
                             <div className="user_img_container">
-                                {userData?.user?.profile_image ?
-                                    <img src={userData?.user?.profile_image} alt="img" loading="lazy" />
+                                {userData?.profile_image ?
+                                    <img src={userData?.profile_image} alt="img" loading="lazy" />
                                     :
                                     <svg className="skeleton-user-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20" >
                                         <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
@@ -44,7 +44,7 @@ export default function AccountUser() {
                     </div>
 
                     <div className="user_info">
-                        <h3 className="user_name">{userData?.user?.name?.split(" ").slice(0, 2).join(" ")}</h3>
+                        <h3 className="user_name">{userData?.name?.split(" ").slice(0, 2).join(" ")}</h3>
                     </div>
                 </div>
             </div>
