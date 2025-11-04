583117cbbe800564391b10e109425cc731461761
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-11-04 23:48:02 +0200

    add url proxy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://api.maaashi.com', // الرابط الأساسي للباك
+        target: 'https://api.maaashi.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api') // يحتفظ بالـ /api في الطلب
       }
     }
   }
