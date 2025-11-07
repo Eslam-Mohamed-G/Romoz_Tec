1608ca52cebc9d355182c3c18000c96ca2e71bb1
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-11-07 13:20:17 +0200

    Adding new modifications

--- src/Pages/UserProfile/UserSettings/UserSettings.jsx
+++ src/Pages/UserProfile/UserSettings/UserSettings.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
 import "./userSettingsStyle.css"
 import { contextData } from '../../../Context/Context';
+import { useFormik } from 'formik';
+import * as Yup from "yup";
 
 export default function UserSettings() {
     const { userID, token, fetchUserData, userData } = useContext(contextData);
@@ -66,17 +68,52 @@ export default function UserSettings() {
     };
 
     useEffect(() => {
-        if (userData?.data?.profile_image) {
-            setProfileImage(`${userData.data.profile_image}?t=${Date.now()}`);
-            setCoverImage(`${userData?.data?.cover_image}?t=${Date.now()}`);
-        }
-    }, [userData]);
-
-    useEffect(() => {
         if (userID && token) {
             fetchUserData();
         }
     }, [userID, token]);
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [serverMessage, setServerMessage] = useState(null);
+    const [isEditMode, setIsEditMode] = useState(false);
+
+    const validationSchema = Yup.object({
+        userName: Yup.string().trim().required("الرجاء إدخال الاسم الكامل"),
+        email: Yup.string().email("بريد إلكتروني غير صالح").required("الرجاء إدخال البريد الإلكتروني"),
+        phone: Yup.string().required("رقم الجوال مطلوب")
+            .matches(
+                /^(05\d{8}|\+?9665\d{8})$/,
+                "يرجى إدخال رقم جوال سعودي صالح يبدأ بـ 05 أو 9665"
+            ),
+    });
+
+    // formik
+    const formik = useFormik({
+        initialValues: {
+            userName: userData?.name || "",
+            email: userData?.email || "",
+            phone: userData?.phone || "",
+        },
+        validationSchema,
+        onSubmit: async (values) => {
+            alert(values);
+        },
+    });
+
+    const { values, errors, touched, handleChange, handleBlur, handleSubmit, setFieldValue } = formik;
+    const isModified =
+        !userData?.name ||
+        values.userName !== userData?.name ||
+        values.email !== userData?.email ||
+        values.phone !== userData?.phone;
+
+    useEffect(() => {
+        if (userData?.profile_image) {
+            setProfileImage(`${userData?.profile_image}?t=${Date.now()}`);
+            setCoverImage(`${userData?.cover_image}?t=${Date.now()}`);
+        }
+    }, [userData]);
+
     return (
         <section className='UserSettings_container'>
             <header className='userProfile_header'>
@@ -99,8 +136,8 @@ export default function UserSettings() {
                     <div className="user_data">
                         <div className="user_images">
                             <div className="cover_user_image">
-                                {userData?.user?.cover_image ?
-                                    <img src={userData?.user?.cover_image} alt="img cover" loading="lazy" />
+                                {userData?.cover_image ?
+                                    <img src={coverImage || userData?.cover_image} alt="img cover" loading='lazy' />
                                     :
                                     <svg className="skeleton-svg" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
                                         <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
@@ -116,7 +153,7 @@ export default function UserSettings() {
                                             handleImageUpload(
                                                 e,
                                                 "cover_image",
-                                                "https://api.mashy.sand.alrmoz.com/api/cover-image"
+                                                "/api/cover-image"
                                             )
                                         }
                                         disabled={imageLoading}
@@ -132,13 +169,18 @@ export default function UserSettings() {
 
                             <div className="profile_user_image">
                                 <div className="user_img_container">
-                                    {userData?.user?.profile_image ?
-                                        <img src={userData?.user?.profile_image} alt="img" loading="lazy" />
+                                    {userData?.profile_image ?
+                                        <img src={profileImage || userData?.profile_image} alt="img" loading="lazy" />
                                         :
                                         <svg className="skeleton-user-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20" >
                                             <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
                                         </svg>
                                     }
+                                    {imageLoading && (
+                                        <div className="upload_overlay">
+                                            <div className="UploadImages_loader" />
+                                        </div>
+                                    )}
                                 </div>
                                 <label className="profile_camera_btn">
                                     <div className="profile_camera_icon">
@@ -152,7 +194,7 @@ export default function UserSettings() {
                                             handleImageUpload(
                                                 e,
                                                 "profile_image",
-                                                "https://api.mashy.sand.alrmoz.com/api/profile-image"
+                                                "/api/profile-image"
                                             )
                                         }
                                         disabled={imageLoading}
@@ -165,8 +207,108 @@ export default function UserSettings() {
                             <h3 className="user_name">{userData?.user?.name?.split(" ").slice(0, 2).join(" ")}</h3>
                         </div>
                     </div>
+
+                    <form className="Settings_user_form">
+                        <div className="input_container">
+                            <label htmlFor="userName" className="input_label">
+                                <span>الاسم</span>
+                                {touched.userName && errors.userName && (
+                                    <p className="error_message">{errors.userName}</p>
+                                )}
+                            </label>
+
+                            <div className="input_field">
+                                <input
+                                    type="text"
+                                    name="userName"
+                                    id="userName"
+                                    value={values.userName}
+                                    onChange={(e) => setFieldValue("userName", e.target.value)}
+                                    placeholder={userData?.name}
+                                    disabled={!isEditMode}
+                                    className={`input ${!isEditMode ? "readonly" : ""}`}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="input_container">
+                            <label htmlFor="email" className="input_label">
+                                <span>بريدك الإكتروني</span>
+                                {touched.email && errors.email && (
+                                    <p className="error_message">{errors.email}</p>
+                                )}
+                            </label>
+
+                            <div className="input_field">
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    value={values.email}
+                                    onChange={(e) => setFieldValue("email", e.target.value)}
+                                    placeholder={userData?.email}
+                                    disabled={!isEditMode}
+                                    className={`input ${!isEditMode ? "readonly" : ""}`}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="input_container">
+                            <label htmlFor="phone" className="input_label">
+                                <span>رقم الجوال</span>
+                                {touched.phone && errors.phone && (
+                                    <p className="error_message">{errors.phone}</p>
+                                )}
+                            </label>
+                            <div className="input_field">
+                                <input
+                                    type="tel"
+                                    name="phone"
+                                    id="phone"
+                                    value={values.phone}
+                                    onChange={(e) => setFieldValue("phone", e.target.value)}
+                                    placeholder={userData?.phone}
+                                    disabled={!isEditMode}
+                                    className={`input ${!isEditMode ? "readonly" : ""}`}
+                                />
+                            </div>
+                        </div>
+                        {/* two buttons */}
+                        <div className="two_buttons">
+                            {isEditMode && (
+                                <button
+                                    type="button"
+                                    className="cancel_button"
+                                    onClick={() => {
+                                        setFieldValue("useName", userData?.name || "");
+                                        setFieldValue("email", userData?.email || "");
+                                        setFieldValue("phone", userData?.phone || "");
+
+                                        setIsEditMode(false);
+                                    }}
+                                >
+                                    الغاء
+                                </button>
+                            )}
+
+                            <button
+                                type="button"
+                                className="submit_btn"
+                                disabled={isLoading || (isEditMode && !isModified)}
+                                onClick={() => {
+                                    if (!isEditMode) {
+                                        setIsEditMode(true);
+                                    } else {
+                                        handleSubmit();
+                                    }
+                                }}
+                            >
+                                {isLoading ? <div className="spinnerLoader" /> : isEditMode ? "حفظ" : "تعديل الملف الشخصي"}
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </section>
     )
-}
+};
