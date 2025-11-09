144811042570b4b7646a68cd8f9e2e329de51e7f
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-11-09 16:32:56 +0200

    add forgotPassword

--- src/Pages/Auth/ForgotPassword/ForgotPassword.jsx
+++ src/Pages/Auth/ForgotPassword/ForgotPassword.jsx
@@ -1,14 +1,53 @@
-import "./forgotPassword.css";
-import { CgMail } from "react-icons/cg";
+import React from 'react'
 import { Link } from "react-router-dom";
+import "./forgotPasswordStyle.css";
 
-const ForgotPassword = () => {
+export default function ForgotPassword() {
   return (
-    <div className="login-wrapper">
-      <div className="login-container">
+    <div className="forgotPassword-wrapper">
+      <div className="top_line" />
+      <div className="forgotPassword-container">
+        <header className='forgotPassword_header'>
+          <div className="header_icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-mail-icon lucide-mail"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" /><rect x={2} y={4} width={20} height={16} rx={2} /></svg>
+          </div>
+          <h2>أدخل بريدك الإلكتروني</h2>
+        </header>
+
+        <div className="forgotPassword-box">
+          <p>
+            "رجاءً أدخل بريدك الإلكتروني المسجّل لتلقي رمز التحقق واستعادة كلمة المرور بسهولة"
+          </p>
+        </div>
+        <form className="forgotPassword_form">
+          <div className="input_container">
+            <header className="input_label">
+              <span>بريدك الإلكتروني</span>
+            </header>
+
+            <div className="input_field">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className='input'
+                placeholder='أدخل بريدك الإلكتروني'
+              />
+              <div className="email_icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-mail-icon lucide-mail"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" /><rect x={2} y={4} width={20} height={16} rx={2} /></svg>
+              </div>
+            </div>
+          </div>
+
+          <button className="forgotPassword_button">
+            إرسال الرمز
+          </button>
+        </form>
+      </div>
+      {/* <div className="forgotPassword-container">
         <h2>تحقق من الكود</h2>
         <div className="verify-box">
-          <CgMail className="verify-icon" />
+          <img src="/Icons/Auth/email.svg" alt="email" className="verify-icon" />
           <p>
             أدخل رمز التحقق المرسل إلى: <b>a*****@gmail.com</b>
           </p>
@@ -27,8 +66,7 @@ const ForgotPassword = () => {
           <br />
           <a href="/contact">تواجه مشكلة؟ تواصل معنا</a>
         </div>
-      </div>
+      </div> */}
     </div>
-  );
-};
-export default ForgotPassword;
+  )
+}
