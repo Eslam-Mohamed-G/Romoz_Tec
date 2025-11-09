2997bdd8de9b42535b5a6ef8cc09bca4bff7499a
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-11-09 16:37:11 +0200

    add resetPasswordStyle.css

--- src/Pages/Auth/ResetPassword/ResetPassword.jsx
+++ src/Pages/Auth/ResetPassword/ResetPassword.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useRef } from "react";
-import "./resetPassword.css";
-import { CgMail } from "react-icons/cg";
 import { Link } from "react-router-dom";
+import "./resetPasswordStyle.css";
 
 const ResetPassword = () => {
   const [otp, setOtp] = useState(["", "", "", "", "", ""]);
@@ -30,7 +29,9 @@ const ResetPassword = () => {
         <h2>تحقق من الكود</h2>
 
         <div className="verify-box">
-          <CgMail className="verify-icon" />
+          <div className="header_icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-mail-icon lucide-mail"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" /><rect x={2} y={4} width={20} height={16} rx={2} /></svg>
+          </div>
           <p>
             أدخل رمز التحقق المرسل إلى: <b>a*****@gmail.com</b>
           </p>
