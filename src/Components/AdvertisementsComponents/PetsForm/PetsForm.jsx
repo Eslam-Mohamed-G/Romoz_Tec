fa6c84fe35695de02a1ca6e5828cb50b53d7ee40
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-11-12 14:43:28 +0200

    add PetsForm

--- src/Components/AdvertisementsComponents/PetsForm/PetsForm.jsx
+++ src/Components/AdvertisementsComponents/PetsForm/PetsForm.jsx
@@ -1,12 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
-import "./PetsForm.css"
-import DropdownField from '../DropdownField/DropdownField';
-const ALL_TAGS = [
-    "أبل", "خيل", "ماعز",
-    "غنم", "بِغاء", "حمام", "قطط", "دجاج",
-    "كلاب", "بقر", "أسماك وسلاحف", "أرانب",
-    "بط", "سناجب", "هامستر"
-];
+import CustomDropdown from '../CustomDropdown/CustomDropdown';
+import { pets } from '../../../data';
 
 export default function PetsForm({ formik }) {
     const { values, setFieldValue, errors } = formik;
@@ -27,12 +21,14 @@ export default function PetsForm({ formik }) {
 
     return (
         <div className="input_container">
-            <label htmlFor="moreInfo">معلومات اضافية
+            <header className="input_label">
+                <span>معلومات اضافية</span>
                 {errors.information?.pets?.animalType && (
                     <div className="info_error">{errors.information?.pets?.animalType}</div>
                 )}
-            </label>
-            <div className="moreInfo_container" ref={tagsDropdownRef}>
+            </header>
+
+            <div className="input_field" ref={tagsDropdownRef}>
                 <input
                     type="text"
                     name="information.pets.animalType"
@@ -40,30 +36,15 @@ export default function PetsForm({ formik }) {
                     onChange={(e) => setFieldValue("information.pets.animalType", e.target.value)}
                     onClick={() => setIsTagsOpen(true)}
                     id="moreInfo"
-                    className='moreInfo_input input'
+                    className='input'
                     placeholder='أدخل معلومات اضافية'
                 />
-                <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-down-icon lucide-chevron-down"><path d="m6 9 6 6 6-6" /></svg>
+                <div className="arrow_up">
+                    <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className={isTagsOpen ? "open" : ""}><path d="m18 15-6-6-6 6" /></svg>
+                </div>
             </div>
 
-            {/* Dropdown options */}
-            {isTagsOpen && (
-                <ul className="input_option">
-                    {ALL_TAGS.map((option, id) => (
-                        <li
-                            key={id}
-                            onMouseDown={(e) => {
-                                e.preventDefault();
-                                setFieldValue("information.pets.animalType", option);
-                                setIsTagsOpen(false);
-                            }}
-                        >
-                            {option}
-                        </li>
-                    )
-                    )}
-                </ul>
-            )}
+            <CustomDropdown isOpen={isTagsOpen} setIsOpen={setIsTagsOpen} data={pets} formik={formik} name="information.pets.animalType" />
         </div>
     )
-}
+};
