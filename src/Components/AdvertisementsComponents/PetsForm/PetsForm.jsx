044daffdf96330f42eb84cd1a58a088c3036d51c
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-11-12 14:43:31 +0200

    add PetsForm

--- src/Components/AdvertisementsComponents/PetsForm/PetsForm.jsx
+++ src/Components/AdvertisementsComponents/PetsForm/PetsForm.jsx
@@ -1,23 +1,10 @@
-import React, { useEffect, useRef, useState } from 'react';
-import CustomDropdown from '../CustomDropdown/CustomDropdown';
+import React, { useState } from 'react';
 import { pets } from '../../../data';
+import CustomDropdown from '../CustomDropdown/CustomDropdown';
 
 export default function PetsForm({ formik }) {
     const { values, setFieldValue, errors } = formik;
-    const [isTagsOpen, setIsTagsOpen] = useState(false);
-    const tagsDropdownRef = useRef(null);
-
-    useEffect(() => {
-        const handleClickOutsidetagesDrop = (event) => {
-            if (tagsDropdownRef.current && !tagsDropdownRef.current.contains(event.target)) {
-                setIsTagsOpen(false);
-            }
-        }
-        document.addEventListener("mousedown", handleClickOutsidetagesDrop);
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutsidetagesDrop);
-        };
-    }, []);
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className="input_container">
@@ -28,23 +15,23 @@ export default function PetsForm({ formik }) {
                 )}
             </header>
 
-            <div className="input_field" ref={tagsDropdownRef}>
+            <div className="input_field">
                 <input
                     type="text"
                     name="information.pets.animalType"
                     value={values.information?.pets?.animalType}
                     onChange={(e) => setFieldValue("information.pets.animalType", e.target.value)}
-                    onClick={() => setIsTagsOpen(true)}
+                    onClick={() => setIsOpen(true)}
                     id="moreInfo"
                     className='input'
                     placeholder='أدخل معلومات اضافية'
                 />
                 <div className="arrow_up">
-                    <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className={isTagsOpen ? "open" : ""}><path d="m18 15-6-6-6 6" /></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className={isOpen ? "open" : ""}><path d="m18 15-6-6-6 6" /></svg>
                 </div>
             </div>
 
-            <CustomDropdown isOpen={isTagsOpen} setIsOpen={setIsTagsOpen} data={pets} formik={formik} name="information.pets.animalType" />
+            <CustomDropdown isOpen={isOpen} setIsOpen={setIsOpen} data={pets} formik={formik} name="information.pets.animalType" />
         </div>
     )
 };
